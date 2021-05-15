3366ce64f47b994bee2775bc8ef07affd302a3e9
Prajwal Pandey <nickey.24nov@gmail.com>
2021-05-15 21:37:16 +0530

    asd

--- PresenceSummarizer/models.py
+++ PresenceSummarizer/models.py
@@ -1,18 +1,5 @@
 from django.db import models
 
-# Create your models here.
-# class Result(models.Model):
-#     name = models.CharField(max_length=20)
-#     timeStamps = models.ManyToManyField("TimeStamps", blank=False)
-#     duration = models.TextField()
-
-#     def __str__(self) -> str:
-#         return f'{name} was present for total duration of {duration}'
-
-# class TimeStamps(models.Model):
-#     case = models.ForeignKey(Result, on_delete=models.CASCADE, related_name='timeStamp')
-#     time = models.TimeField()
-
 class ScanRequest(models.Model):
     name = models.CharField(max_length = 40)
     img = models.ImageField(upload_to = 'images/')
